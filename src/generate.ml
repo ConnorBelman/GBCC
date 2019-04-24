68d0182920870eaa9f81a3263ccf18e12ff74396
Connor Belman <connorbelman@gmail.com>
2019-04-24 01:01:14 -0400

    rewrote equality and unary operator  assembly generation

--- src/generate.ml
+++ src/generate.ml
@@ -1,12 +1,24 @@
 open CTypes
 open Printf
 
+let fresh =
+  let cntr = ref 0 in
+  fun () ->
+    cntr := !cntr + 1 ;
+    !cntr
+
 let rec parseRet expr file =
     match expr with
     | Constant(x) -> fprintf file "ld a,$%02X\n\t" x
-    (* | Constant(x) -> x
-    | Neg(x) ->255 land (-(parseRet x))
-    | BitComp(x) -> 255 land (lnot (parseRet x)) *)
+    (* | Constant(x) -> x *)
+    | Neg(x) ->
+        parseRet x file;
+        fprintf file "cpl\n\tinc a\n\t"
+    (* | Neg(x) -> 255 land (-(parseRet x)) *)
+    | BitComp(x) ->
+        parseRet x file;
+        fprintf file "cpl\n\t"
+    (* | BitComp(x) -> 255 land (lnot (parseRet x)) *)
     | BitOr(x, y) ->
         parseRet y file;
         fprintf file "push af\n\t";
@@ -40,13 +52,24 @@ let rec parseRet expr file =
         fprintf file "pop bc\n\tsub b\n\t"
     (* | Sub(x, y) -> 255 land (parseRet x - parseRet y)
     | Mul(x, y) -> 255 land (parseRet x * parseRet y)
-    | Div(x, y) -> 255 land (parseRet x / parseRet y)
-    | Not(x) -> if parseRet x = 0 then 1 else 0
+    | Div(x, y) -> 255 land (parseRet x / parseRet y) *)
+    (* | Not(x) -> if parseRet x = 0 then 1 else 0
     | And(x, y) -> if (parseRet x != 0) && (parseRet y != 0) then 1 else 0
-    | Or(x, y) -> if (parseRet x != 0) || (parseRet y != 0) then 1 else 0
-    | Equal(x, y) -> if parseRet x = parseRet y then 1 else 0
-    | NotEqual(x, y) -> if parseRet x != parseRet y then 1 else 0
-    | Less(x, y) -> if parseRet x < parseRet y then 1 else 0
+    | Or(x, y) -> if (parseRet x != 0) || (parseRet y != 0) then 1 else 0 *)
+    | Equal(x, y) ->
+        let j = fresh() in
+        parseRet y file;
+        fprintf file "push af\n\t";
+        parseRet x file;
+        fprintf file "pop bc\n\tcp b\n\tld a,$00\n\tjp nz,_cp%d\n\tld a,$01\n_cp%d\n\tret\n\t" j j
+    (* | Equal(x, y) -> if parseRet x = parseRet y then 1 else 0 *)
+    | NotEqual(x, y) ->
+        parseRet y file;
+        fprintf file "push af\n\t";
+        parseRet x file;
+        fprintf file "pop bc\n\tsub b\n\t"
+    (* | NotEqual(x, y) -> if parseRet x != parseRet y then 1 else 0 *)
+    (* | Less(x, y) -> if parseRet x < parseRet y then 1 else 0
     | Greater(x, y) -> if parseRet x > parseRet y then 1 else 0
     | LessEqual(x, y) -> if parseRet x <= parseRet y then 1 else 0
     | GreaterEqual(x, y) -> if parseRet x >= parseRet y then 1 else 0 *)
